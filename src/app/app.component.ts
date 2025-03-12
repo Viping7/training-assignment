66ba15e80930ac4c9d8b41f212ab5e6f5f38bb51
Vipin Gopinath <vipin.gopinath@hitachids.com>
2025-03-12 16:25:57 +0530

    Assignment8 - Modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { JokeComponent } from './components/joke/joke.component';
+import { SharedModule } from './shared/shared.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, SharedModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
